33e64dad08efbfc5c3a8c0ee0b04c8a83091616a
Daniel Schwabacher <danielschwabacher@gmail.com>
2018-05-02 18:30:10 -0600

    fixed drag and drop bugs, added dragula css styles.

--- public/scripts/arduino/arduino-element-base.js
+++ public/scripts/arduino/arduino-element-base.js
@@ -1,39 +1,13 @@
 var socket = io.connect('http://localhost:' + 3000);
-
-// target elements with the "draggable" class
-interact('.arduino-element-block')
-  .draggable({
-    // enable inertial throwing
-    inertia: true,
-    // call this function on every dragmove event
-    onmove: onmove_translater
+window.addEventListener('load', function(){
+    containers = [document.getElementById('element-area'), document.getElementById('bp-area')]
+    dragula(containers, {
+        copy: true,
+        copy: function (el, source) {
+            return source === document.getElementById('element-area')
+        },
+        accepts: function (el, target) {
+            return target == document.getElementById('bp-area')
+        }
+    }); 
 });
-
-
-function onmove_translater(event) {
-    if (!event.target.dragOrigin) {
-        var clone = event.target.cloneNode(true);
-        clone.dragOrigin = event.target;
-        event.interaction.element = clone;
-        event.interaction.dragging = false;
-        dragTarget = clone;
-        // dragTarget.setAttribute('data-x', pos.left);
-        // dragTarget.setAttribute('data-y', pos.top);
-        document.body.appendChild(clone);
-    } 
-    else {
-        dragTarget = event.target;
-    }
-
-      // keep the dragged position in the data-x/data-y attributes
-      x = (parseFloat(dragTarget.getAttribute('data-x')) || 0) + event.dx,
-      y = (parseFloat(dragTarget.getAttribute('data-y')) || 0) + event.dy;
-  
-    // translate the element
-    dragTarget.style.webkitTransform =
-    dragTarget.style.transform =
-      'translate(' + x + 'px, ' + y + 'px)';
-    // update the posiion attributes
-    dragTarget.setAttribute('data-x', x);
-    dragTarget.setAttribute('data-y', y);
-}
--- public/scripts/arduino/dropzone-base.js
+++ public/scripts/arduino/dropzone-base.js
@@ -4,10 +4,10 @@ function create_hook(target){
     console.log(block_type);
     switch (block_type){
         case "arduino-led-toggle":
-            target.led_param = prompt("Which pin controls this LED")
+            // target.led_param = prompt("Which pin controls this LED")
             target.addEventListener('click', led_toggle)
         case "arduino-data-reader":
-            target.data_read_pin = prompt("Which pin to read data from");
+            // target.data_read_pin = prompt("Which pin to read data from");
             target.addEventListener('click', toggle_read)
     }
 }
@@ -21,27 +21,3 @@ function remove_hook(target){
             target.removeEventListener('click', read_toggle)
     }
 }
-
-// enable draggables to be dropped into this
-interact('.bp-area').dropzone({
-    // only accept elements matching this CSS selector
-    accept: '.arduino-element-block',
-    // Require a 75% element overlap for a drop to be possible
-    overlap: 0.80,
-    ondragenter: function (event) {
-        event.relatedTarget.classList.add('can-drop');
-        event.relatedTarget.classList.remove('was-dropped');
-    },
-    ondragleave: function (event) {
-      event.relatedTarget.classList.remove('was-dropped');
-      event.relatedTarget.classList.remove('can-drop');
-      remove_hook(event.relatedTarget)
-    },
-    ondrop: function(event){
-      event.relatedTarget.classList.remove('can-drop');
-      event.relatedTarget.classList.add('was-dropped');
-      create_hook(event.relatedTarget)
-      event.relatedTarget.classList.remove('arduino-element-block')
-    }
-  });
-  
