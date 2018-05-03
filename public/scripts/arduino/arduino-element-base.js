2f667b9d07bbc6adeb556b948a436a15ae6b7dab
Daniel Schwabacher <danielschwabacher@gmail.com>
2018-05-02 19:39:19 -0600

    fixed drag and display. added event listeners on generated spans.

--- public/scripts/arduino/arduino-element-base.js
+++ public/scripts/arduino/arduino-element-base.js
@@ -1,4 +1,26 @@
 var socket = io.connect('http://localhost:' + 3000);
+
+function show_element_spans(el){
+    var spans = el.getElementsByTagName('span')
+    i = 0;
+    var num_spans = 2
+    while (i < num_spans){
+        if (spans[i].id == 'not-dropped'){
+            spans[i].removeAttribute('not-dropped');
+        }
+        spans[i].id = 'was-dropped'
+        add_span_event_listeners(spans[i])
+        i++
+    }
+}
+
+
+function add_span_event_listeners(el){
+    el.addEventListener('click', function(){
+       console.log("clicked: " + JSON.stringify(el.innerHTML))
+    });
+}
+
 window.addEventListener('load', function(){
     containers = [document.getElementById('element-area'), document.getElementById('bp-area')]
     dragula(containers, {
@@ -9,5 +31,8 @@ window.addEventListener('load', function(){
         accepts: function (el, target) {
             return target == document.getElementById('bp-area')
         }
-    }); 
+    }).on('drop', function(el, container, source){
+        show_element_spans(el);
+    });
 });
+
