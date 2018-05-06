c5a3eb135c660597583cb73fedd996f4f9ec340b
Daniel Schwabacher <danielschwabacher@gmail.com>
2018-05-05 18:41:45 -0600

    led toggle base class organization.

--- public/scripts/arduino/arduino-element-base.js
+++ public/scripts/arduino/arduino-element-base.js
@@ -1,25 +1,5 @@
 var socket = io.connect('http://localhost:' + 3000);
 
-function show_element_spans(el){
-    var spans = el.getElementsByTagName('span')
-    i = 0;
-    var num_spans = 2
-    while (i < num_spans){
-        if (spans[i].id == 'not-dropped'){
-            spans[i].removeAttribute('not-dropped');
-        }
-        spans[i].id = 'was-dropped'
-        add_span_event_listeners(spans[i])
-        i++
-    }
-}
-
-
-function add_span_event_listeners(el){
-    el.addEventListener('click', function(){
-       console.log("clicked: " + JSON.stringify(el.innerHTML))
-    });
-}
 
 window.addEventListener('load', function(){
     containers = [document.getElementById('element-area'), document.getElementById('bp-area')]
@@ -32,7 +12,10 @@ window.addEventListener('load', function(){
             return target == document.getElementById('bp-area')
         }
     }).on('drop', function(el, container, source){
-        show_element_spans(el);
+        var element_type = el.classList[1];
+        if (element_type == "arduino-led-toggle"){
+            new_toggle = new LedToggle(el);
+        }
     });
 });
 
